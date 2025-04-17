ebc7eb050e0d850ac48a6c6541ec28d3dbe3c403
mohammad <mohammadmunti.5@gmail.com>
2025-04-17 14:02:36 +0530

    addexpiry getallexpiry controller, database, router setup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import billingRouter from './routes/bills.js'
 import purchaseRouter from './routes/purchase.js'
 import paymentRouter from './routes/payments.js'
 import addSupplierRouter from './routes/addsupplier.js'
+import expiryRouter from './routes/expiry.js'
 
 
 import cors from 'cors'
@@ -24,6 +25,7 @@ app.use('/api/bills',billingRouter)
 app.use('/api/purchase',purchaseRouter)
 app.use('/api/payment',paymentRouter)
 app.use('/api/addSupplier',addSupplierRouter)
+app.use('/api/expiry',expiryRouter)
 
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`);

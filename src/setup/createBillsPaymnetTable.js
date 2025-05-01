6b1f87de5b8c176b125874b0e5185c3859e89f20
mohammad <mohammadmunti.5@gmail.com>
2025-05-01 09:41:51 +0530

    created db connection and table structure for bill and payment

---
+++ src/setup/createBillsPaymnetTable.js
@@ -0,0 +1,41 @@
+import db from '../db/billPayment.js'
+
+db.exec(`
+CREATE TABLE IF NOT EXISTS bills (
+  id INTEGER PRIMARY KEY,
+  invoice TEXT,
+  date TEXT,
+  supplierName TEXT,
+  supplierDrugLn TEXT,
+  supplierContact TEXT,
+  totalAmount REAL,
+  totalGst REAL,
+  totalDiscount REAL,
+  total REAL
+);
+
+CREATE TABLE IF NOT EXISTS bill_products (
+  id INTEGER PRIMARY KEY,
+  billId INTEGER,
+  name TEXT,
+  batchNumber TEXT,
+  expiryDate TEXT,
+  stock INTEGER,
+  unitPrice REAL,
+  mrp REAL,
+  discount REAL,
+  gstPercentage REAL,
+  supplier TEXT,
+  category TEXT,
+  FOREIGN KEY (billId) REFERENCES bills(id)
+);
+
+CREATE TABLE IF NOT EXISTS payments (
+  id INTEGER PRIMARY KEY,
+  date TEXT,
+  invoice TEXT,
+  supplierName TEXT,
+  drugLicenseNumber TEXT,
+  total REAL
+);
+`);
